7baaa0f6c2904a05998ed7c6412f2f203ee4427c
Filipp Voronov <fivoronov@gmail.com>
2022-02-15 21:08:58 +0300

    maxGap added

--- src/main/java/StatisticsService.java
+++ src/main/java/StatisticsService.java
@@ -7,4 +7,17 @@ public class StatisticsService {
         }
         return sum;
     }
+
+    public int maxGap(int[] temps) {
+        int maxGap = 0;
+        for (int i = 0; i < temps.length - 1; i++) {
+            int firstTemp = temps[i];
+            int secondTemp = temps[i + 1];
+            int gap = Math.abs(firstTemp - secondTemp);
+            if (gap > maxGap) {
+                maxGap = gap;
+            }
+        }
+        return maxGap;
+    }
 }
