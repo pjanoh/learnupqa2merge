21583d93135700a337f3a123cd24c6076f123377
Filipp Voronov <fivoronov@gmail.com>
2022-02-15 21:17:06 +0300

    Super max method added

--- src/main/java/StatisticsService.java
+++ src/main/java/StatisticsService.java
@@ -7,4 +7,20 @@ public class StatisticsService {
         }
         return sum;
     }
+
+    public int max(String[] texts) {       // "24 5 6 77"
+        int max = 0;
+
+        for (String text : texts) { // "24 5 6 77"
+            String[] parts = text.split(" ");   // ["24", "5", "6", "77"]
+            for (String part : parts) {
+                int x = Integer.parseInt(part); // "24" -> 24
+                if (x > max) {
+                    max = x;
+                }
+            }
+        }
+
+        return max;
+    }
 }
